2dfee467d2e996da2aa32d7593e3c2c191951255
Raresu8636 <134927346+raresu8636@users.noreply.github.com>
2025-03-28 09:34:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)'
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -171,17 +172,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('aside img');
 
     images.forEach(img => {
-        img.addEventListener('mouseenter', function() {
-            this.classList.add('hovered');
-        });
-
-        img.addEventListener('mouseleave', function() {
-            this.classList.remove('hovered');
-        });
-
         img.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the link from opening
-
+            event.preventDefault();
             this.classList.toggle('hovered');
         });
     });
